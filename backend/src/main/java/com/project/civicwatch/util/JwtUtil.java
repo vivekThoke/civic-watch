f3f8ac5ee9a96b015401c7ec882b33f8440295f4
vivekThoke <vivekthoke5@gmail.com>
2026-01-04 10:23:07 +0530

    add auth endpoint for register and login with jwt

--- backend/src/main/java/com/project/civicwatch/util/JwtUtil.java
+++ backend/src/main/java/com/project/civicwatch/util/JwtUtil.java
@@ -2,14 +2,22 @@ package com.project.civicwatch.util;
 
 import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
 import io.jsonwebtoken.Jwts;
 
+import java.security.Key;
+import java.util.Base64;
 import java.util.Date;
 
 @Component
 public class JwtUtil {
-    private final String SECRET_KEY = "civicwatch-secret-key";
+    private final String SECRET_KEY = "my-secret-key-which-should-be-long";
+
+    private Key getSigningKey(){
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
+
 
     public String generateToke(String email, String role) {
         return Jwts.builder()
@@ -17,13 +25,14 @@ public class JwtUtil {
                 .claim("role", role)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + 86400000))
-                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
     public String extractEmail(String token) {
-        return Jwts.parser()
-                .setSigningKey(SECRET_KEY)
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
                 .parseClaimsJws(token)
                 .getBody()
                 .getSubject();
