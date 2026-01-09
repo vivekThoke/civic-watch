b7ddc5ffb4d689710ee67266cadfc1eff231e581
vivekThoke <vivekthoke5@gmail.com>
2026-01-09 21:02:35 +0530

    added endpoint for upvote and remove upvote

--- backend/src/main/java/com/project/civicwatch/controller/IssueController.java
+++ backend/src/main/java/com/project/civicwatch/controller/IssueController.java
@@ -5,12 +5,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.project.civicwatch.dto.IssueCreateRequest;
 import com.project.civicwatch.dto.IssueDetailResponse;
 import com.project.civicwatch.dto.IssueResponse;
+import com.project.civicwatch.dto.UpvoteResponse;
 import com.project.civicwatch.model.Issue;
 import com.project.civicwatch.model.IssueStatus;
 import com.project.civicwatch.model.User;
 import com.project.civicwatch.repository.UserRepository;
 import com.project.civicwatch.service.IssueFeedService;
 import com.project.civicwatch.service.IssueService;
+import com.project.civicwatch.service.IssueUpvoteService;
 import jdk.jfr.ContentType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -33,6 +35,7 @@ public class IssueController {
     private final UserRepository userRepository;
     private final ObjectMapper objectMapper;
     private final IssueFeedService issueFeedService;
+    private final IssueUpvoteService issueUpvoteService;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public IssueResponse createIssue(@RequestPart(value = "data") String data,
@@ -76,7 +79,20 @@ public class IssueController {
         Page<Issue> issues = issueFeedService.getPublicFeed(categoryId, status, locality, pageable);
 
         return issues.map(IssueResponse::from);
-
-
     }
+
+    @PostMapping("/{id}/upvote")
+    public ResponseEntity<UpvoteResponse> upVote(@PathVariable Long id, Principal principal){
+        return ResponseEntity.ok(
+                issueUpvoteService.upVote(id, principal.getName())
+        );
+    }
+
+    @DeleteMapping("/{id}/upvote")
+    public ResponseEntity<UpvoteResponse> deleteUpvote(@PathVariable Long id, Principal principal){
+        return ResponseEntity.ok(
+                issueUpvoteService.removeUpVote(id, principal.getName())
+        );
+    }
+
 }
--- backend/src/main/java/com/project/civicwatch/dto/UpvoteResponse.java
+++ backend/src/main/java/com/project/civicwatch/dto/UpvoteResponse.java
@@ -6,6 +6,6 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class UpvoteResponse {
-    private Long id;
+    private Long upvotes;
     private boolean upVotedByMe;
 }
