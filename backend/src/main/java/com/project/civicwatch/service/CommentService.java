ebd4fe1f4269462af7ce3961e701abd2e88d7a44
vivekThoke <vivekthoke5@gmail.com>
2026-01-12 19:28:30 +0530

    added Comment service interface

---
+++ backend/src/main/java/com/project/civicwatch/service/CommentService.java
@@ -0,0 +1,13 @@
+package com.project.civicwatch.service;
+
+import com.project.civicwatch.dto.CommentRequest;
+import com.project.civicwatch.dto.CommentResponse;
+
+import java.util.List;
+
+public interface CommentService {
+    CommentResponse addComment(Long issueId, String email, CommentRequest request);
+
+    List<CommentResponse> getCommentByIssue(Long issueId);
+    
+}
